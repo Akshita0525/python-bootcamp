ebd5a1b952db945dd156559a5483211805ba08ac
Akshita0525 <akshitha25aki@gmail.com>
2024-07-24 09:32:09 +0530

    python day5
    day5

--- hw.py
+++ hw.py
@@ -9,4 +9,46 @@
 #***
 #  ***      parallelogram
 #    ***
-#number pyramid
+#number pyramid 
+
+'''rows = 7
+for i in range (1,rows + 1):
+    for j in range (1,rows - i + 1):
+        print (end=" ")
+    for j in range (1,rows + 1):
+        print ("*",end=" ")
+    print()'''
+
+'''rows = 7
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print("* ", end=" ")
+    print()'''
+
+
+'''rows = 3
+cols = 4
+for i in range(0, rows):
+    for j in range(1, i+1):
+        print(" ", end="")
+    for j in range(1, cols):
+        print("*", end="")
+    print()'''
+
+'''rows= 7       
+for i in range(0, rows):       
+        for j in range(0, i + 1):       
+            print("* ", end="")       
+        print()'''
+
+
+'''rows = 7
+for i in range (1,rows + 1):
+    for j in range (1,rows - i + 1):
+        print (end=" ")
+    for j in range (1,rows + 1):
+        print ("*",end=" ")
+    print()'''
+
+
+
